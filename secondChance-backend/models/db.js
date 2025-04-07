19e6cc773cd5dc0d6e9350e9d2d3b952987258bd
iKamranShahzad <mailkamranshahzad@gmail.com>
2025-04-07 11:04:53 -0400

    Added API Endpoints

--- secondChance-backend/models/db.js
+++ secondChance-backend/models/db.js
@@ -15,14 +15,10 @@ async function connectToDatabase() {
 
     const client = new MongoClient(url);      
 
-    // Task 1: Connect to MongoDB
-    // {{insert code}}
+    await client.connect();
+    dbInstance = client.db(dbName)
 
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
-
-    // Task 3: Return database instance
-    // {{insert code}}
+    return dbInstance;
 }
 
 module.exports = connectToDatabase;
